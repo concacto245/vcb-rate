3fac42321f802a1bf21d83b4d2e1cf4cda1621bb
concacto245 <concacto39@gmail.com>
2026-02-23 06:31:05 +0000

    Commit changes

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+
+const app = express();
+
+app.get("/vcb", async (req, res) => {
+
+const browser = await puppeteer.launch({
+args: ['--no-sandbox','--disable-setuid-sandbox']
+});
+
+const page = await browser.newPage();
+
+await page.goto(
+"https://www.vietcombank.com.vn/vi-VN/KHCN/Cong-cu-Tien-ich/KHCN---Lai-suat",
+{ waitUntil: "networkidle2" }
+);
+
+await page.waitForSelector("table");
+
+const data = await page.evaluate(()=>{
+
+const rows=document.querySelectorAll("table tbody tr");
+
+let result=[];
+
+rows.forEach(r=>{
+
+const cols=r.querySelectorAll("td");
+
+result.push({
+month: cols[0].innerText,
+rate: cols[1].innerText
+});
+
+});
+
+return result;
+
+});
+
+await browser.close();
+
+res.json(data);
+
+});
+
+app.listen(3000);
